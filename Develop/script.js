a1c7d86e8f1f073002aecb399ad5a64ee0918bb3
dave-ready <73320561+dave-ready@users.noreply.github.com>
2020-11-21 21:42:32 -0500

    Commit with pseudo and vars

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,25 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Password Criteria
+
+//must be between 8 and 128 characters long
+
+//must include character types lowercase, uppercase, numeric 
+//and/or special characters
+
+
+
+//Input should be validated and at least one character type should 
+//be selected
+
+//after all prompts are answered, a password needs to be generated and 
+//displayed as a prompt or on screen
+
+var psswrdLength
+var chrctrTypes 
+var lwrcase 
+var upprCase 
+var nmbr
+var chrctr 
