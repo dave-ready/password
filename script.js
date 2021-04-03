82666b4ae60342c02c3189b93cbc74e3e518c399
David Ready <davis_ready@hotmail.com>
2021-04-03 19:18:16 -0400

    added more variables

--- script.js
+++ script.js
@@ -1,37 +1,54 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 //Password Criteria
-
 //must be between 8 and 128 characters long
-
 //must include character types lowercase, uppercase, numeric 
 //and/or special characters
-
-
-
 //Input should be validated and at least one character type should 
 //be selected
-
 //after all prompts are answered, a password needs to be generated and 
 //displayed as a prompt or on screen
 
+
+
+// Write password to the #password input
+//function writePassword() {
+//  var password = generatePassword();
+//  var passwordText = document.querySelector("#password");
+
+//  passwordText.value = password;
+
+//}
+
+//Create function to generate password s
+//function generatePassword() {
+
+// Add event listener to generate button
+//generateBtn.addEventListener("click", writePassword);
+
+//};
+
+
+
 var psswrdLength = prompt("Welcome To Password Generator! How many characters would you like your password to be?");
 var confirmLwrCase = confirm("Would you like to use lower case letters?");
-var confirmUpprCase = confirm("Would youi like to use upper-case letters?");
+var confirmUpprCase = confirm("Would you like to use upper-case letters?");
 var confirmNumChar = Confirm ("Would you like to use and numbers or special characters?");
+
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+var upperCase = ["A", "B", "C","D","E", "F", "G", "H", "I","J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y","Z"];
+
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var spclChars = ["!", "@","#","$","%","&", "*", "'", "-", "_", "+", "=", ",", ".", "/", "?", "<", ">", "~"];
+
+var password = [];
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+var getUpperCase = function (x) {
+  return x.toUpperCase ();
+};
 
 
 
@@ -59,6 +76,7 @@ if (confirmUpprCase) {
 if (confirmNumChar) {
   
 }
+
 
 
 
